bc2324deadc3251e38c9b50d21ef89f452bb4e81
tayreardo <tayla_reardon@hotmail.com>
2025-05-16 15:46:21 +1000

    heeeeelp

---
+++ Item.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private string itemName;
+
+    [SerializeField]
+    private int quantity;
+
+    [SerializeField]
+    private Sprite sprite;
+
+    private InventoryManager inventoryManager;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        inventoryManager = GameObject.Find("InventoryCanvas").GetComponent<InventoryManager>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag=="Player")
+        {
+            inventoryManager.AddItem(itemName, quantity, sprite);
+            Destroy(gameObject);
+        }
+    }
+
+
+}
